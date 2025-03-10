22f65dcf9921c8c38bfe8cf9d14e5f8d98c06ca7
niknovikov19 <nikknovikov@gmail.com>
2025-03-10 01:35:58 -0400

    Implement plotting of selected 1-d slices of 2-d matrices

--- proto/ir_mapping/plot_ou_r_cv_matrices.py
+++ proto/ir_mapping/plot_ou_r_cv_matrices.py
@@ -2,6 +2,28 @@ from pathlib import Path
 import pickle
 
 import matplotlib.pyplot as plt
+import numpy as np
+from skimage import measure
+
+
+# Contour function: y = k * x
+def contour_func_const_ratio(x, y, k):
+    return y - k * x
+
+# Find contours: func(x, y) = level
+def find_contours(func, x, y, level=0, **kwargs):
+    xx, yy = np.meshgrid(x, y)
+    z = func(xx, yy, **kwargs)
+    C_idx = measure.find_contours(z, level)
+    C = []
+    for n, c_idx in enumerate(C_idx):
+        c_idx = c_idx.round().astype(int)
+        c = np.full_like(c_idx, np.nan, dtype=float)
+        c[:, 0] = y[c_idx[:, 0]]
+        c[:, 1] = x[c_idx[:, 1]]
+        C.append(c)
+        C_idx[n] = c_idx
+    return C, C_idx
 
 
 dirpath_base = Path('D:\\WORK\\Salvador\\repo\\model_tuner\\test_data\\a1_ou_unconn\\scott_2025_02_28')
@@ -15,6 +37,8 @@ with open(fpath_in, 'rb') as file:
 
 pop_names = list(data['rate'].keys())
 
+#pop_names = ['IT3']
+
 for m, pop_name in enumerate(pop_names):
 
     print(f'{m} {pop_name}...')
@@ -24,8 +48,14 @@ for m, pop_name in enumerate(pop_names):
 
     Xvis = {'Firing rate': R, 'CV': CV}
 
+    # Coordinates
     ouamp_vec = R.columns.values * 100
     oustd_vec = R.index.values * 100
+
+    # Find contour: oustd / ouamp = const
+    k = 0.4
+    C, C_idx = find_contours(contour_func_const_ratio, ouamp_vec, oustd_vec, k=k)
+    C, C_idx = C[0], C_idx[0]
 
     plt.figure(111, figsize=(10, 8))
     plt.clf()
@@ -40,7 +70,10 @@ for m, pop_name in enumerate(pop_names):
         ext = (ouamp_vec[0], ouamp_vec[-1], oustd_vec[0], oustd_vec[-1])
         plt.imshow(X, aspect='auto', cmap='viridis', origin='lower', extent=ext, **par)
         plt.colorbar()
-        plt.plot(ouamp_vec, 0.4 * ouamp_vec, 'k--', label='std = 0.4 * amp')
+        for k in range(0, len(oustd_vec), 4):
+            plt.plot((ouamp_vec.min(), ouamp_vec.max()), (oustd_vec[k], oustd_vec[k]), '--')
+        #plt.plot(ouamp_vec, 0.4 * ouamp_vec, 'k--', label='std = 0.4 * amp')
+        plt.plot(C[:, 1], C[:, 0], 'k--', label=f'std = {k} * amp')
         #plt.legend()
         if n == 1:  plt.xlabel('ouamp * 100')
         plt.ylabel('oustd * 100')
@@ -50,7 +83,12 @@ for m, pop_name in enumerate(pop_names):
 
         plt.subplot(2, 2, 2 * n + 2)
         for k in range(0, len(oustd_vec), 4):
-            plt.plot(ouamp_vec, X.iloc[k, :]) #, label=f"std={oustd}")
+            plt.plot(ouamp_vec, X.iloc[k, :], '--') #, label=f"std={oustd}")
+        ouamp_idx = C_idx[:, 1]
+        oustd_idx = C_idx[:, 0]
+        xx = [X.iloc[oustd_idx[n], ouamp_idx[n]]
+              for n in range(len(ouamp_idx))]
+        plt.plot(ouamp_vec[ouamp_idx], xx, 'k', lw=2)
         if n == 1: plt.xlabel('ouamp * 100')
         plt.ylabel(xname)
         plt.title(f'{pop_name}: {xname}')
@@ -64,4 +102,4 @@ for m, pop_name in enumerate(pop_names):
 
     #break
 
-#input('Press Enter to close the plot...')
+#input('Press any key...')
