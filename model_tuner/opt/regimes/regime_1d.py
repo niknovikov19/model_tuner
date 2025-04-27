82b70e6032177aa6cc0d7aff952011b72ce9e93d
niknovikov19 <nikknovikov@gmail.com>
2025-04-27 04:11:18 -0400

    Add conversion between NetRegime1DList and xarray, refactor NetRegimeWC

--- model_tuner/opt/regimes/regime_1d.py
+++ model_tuner/opt/regimes/regime_1d.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import numpy as np
+import xarray as xr
 
 from .regime_base import PopRegime, NetRegime, NetRegimeList
 
@@ -63,12 +64,27 @@ class NetRegime1DList(NetRegimeList):
         """Returns (pops x regimes) matrix. """
         return self.get_pop_attr_mat('value')
     
+    def to_xr(
+            self,
+            dim_name: str = 'regime',
+            labels: List | None = None
+            ) -> xr.DataArray:
+        """Returns (pops x regimes) xarray. """
+        if labels is None:
+            labels = np.arange(len(self))
+        return xr.DataArray(
+            self.get_pop_regimes_mat(),
+            dims=['pop', dim_name],
+            coords={'pop': self.get_pop_names(), dim_name: labels}
+        )
+    
     @classmethod
     def from_regimes_mat(
             cls,
             pop_names: List[str],
             regimes_mat: np.ndarray  # pops x regimes
             ) -> 'NetRegime1DList':
+        """Creates from (pops x regimes) matrix. """
         L = NetRegime1DList()
         for n in range(regimes_mat.shape[1]):
             regime_vals = regimes_mat[:, n]
@@ -76,6 +92,40 @@ class NetRegime1DList(NetRegimeList):
                 NetRegime1D.from_values(pop_names, regime_vals)
             )
         return L
+    
+    @classmethod
+    def from_xr(
+            cls,
+            R: xr.DataArray,
+            pop_dim: str | None = None,
+            regime_dim: str | None = None
+            ) -> 'NetRegime1DList':
+        """Creates from 2D xarray. """
+        if R.ndim != 2:
+            raise ValueError('R should be 2D')
+        if pop_dim and pop_dim not in R.dims:
+            raise ValueError(f'{pop_dim} not in R.dims')
+        if regime_dim and regime_dim not in R.dims:
+            raise ValueError(f'{regime_dim} not in R.dims')
+        if not pop_dim and not regime_dim:
+            if 'pop' in R.dims:
+                pop_dim = 'pop'
+            if 'regime' in R.dims:
+                regime_dim = 'regime'
+        if not pop_dim and not regime_dim:
+            raise ValueError('Cannot interpret R.dims')
+        if not regime_dim and pop_dim in R.dims:
+            regime_dim = [d for d in R.dims if d != pop_dim][0]
+        if not pop_dim and regime_dim in R.dims:
+            pop_dim = [d for d in R.dims if d != regime_dim][0]
+        if pop_dim == R.dims[0]:
+            R_ = R.values
+        else:
+            R_ = R.values.T
+        return cls.from_regimes_mat(
+            pop_names=R.coords[pop_dim].values,
+            regimes_mat=R_
+        )
     
     @classmethod
     def mix(
--- model_tuner/opt/regimes/regime_wc.py
+++ model_tuner/opt/regimes/regime_wc.py
@@ -1,42 +1,62 @@
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import numpy as np
+import xarray as xr
 
 from model_tuner.utils import from_dict_or_dataclass
 
 from .regime_base import PopRegime, NetRegime, NetRegimeList
+from .regime_1d import PopRegime1D, NetRegime1D, NetRegime1DList
 
 
 @dataclass        
-class PopRegimeWC(PopRegime):
-    r: float = 0
+class PopRegimeWC(PopRegime1D):
+    @property
+    def r(self) -> float:
+        return self.value
+    
+    @r.setter
+    def r(self, new_value: float) -> None:
+        self.value = new_value
 
 
 @dataclass
-class NetRegimeWC(NetRegime):    
-    def __init__(self, pop_regimes: Dict[str, PopRegimeWC | dict] = None):
+class NetRegimeWC(NetRegime1D):    
+    """ def __init__(self, pop_regimes: Dict[str, PopRegimeWC | dict] = None):
         pop_regimes = pop_regimes or {}
         self.pop_regimes = {
             pop_name: from_dict_or_dataclass(pop_regime, PopRegimeWC)
             for pop_name, pop_regime in pop_regimes.items()
-        }
+        } """
 
     def get_pop_rate(self, pop_name: str) -> float:
-        return self.pop_regimes[pop_name].r
+        return self.get_pop_regime_val(pop_name)
     
     def get_pop_rates_vec(self) -> np.ndarray:
-        return self.get_pop_attr_vec('r')
+        return self.get_pop_regimes_vec()
     
     @classmethod
-    def from_rates(cls, pop_names: List[str], pop_rates: List[float]) -> 'NetRegimeWC':
-        R = NetRegimeWC()
-        for pop_name, r in zip(pop_names, pop_rates):
-            R.pop_regimes[pop_name] = PopRegimeWC(r=r)
+    def _convert_parent(cls, R: NetRegime) -> 'NetRegimeWC':
+        R.__class__ = NetRegimeWC
+        for r in R.pop_regimes.values():
+            r.__class__ = PopRegimeWC
         return R
+
+    @classmethod
+    def from_rates(
+            cls,
+            pop_names: List[str],
+            pop_rates: List[float]
+            ) -> 'NetRegimeWC':
+        R = NetRegime1D.from_values(pop_names, pop_rates)
+        return cls._convert_parent(R)
     
     @classmethod
-    def from_rates_dict(cls, pop_rates: Dict[str, float]) -> 'NetRegimeWC':
+    def from_rates_dict(
+            cls,
+            pop_rates: Dict[str, float]
+            ) -> 'NetRegimeWC':
         return cls.from_rates(
             pop_names=list(pop_rates.keys()),
             pop_rates=list(pop_rates.values())
@@ -44,7 +64,36 @@ class NetRegimeWC(NetRegime):
 
 
 @dataclass
-class NetRegimeWCList(NetRegimeList):
+class NetRegimeWCList(NetRegime1DList):
+
     def get_pop_rates_mat(self) -> np.ndarray:
         """Returns (pops x regimes) matrix. """
-        return self.get_pop_attr_mat('r')
+        return self.get_pop_regimes_mat()
+    
+    @classmethod
+    def _convert_parent(cls, L: NetRegimeList) -> 'NetRegimeWCList':
+        L.__class__ = NetRegimeWCList
+        for R in L.net_regimes:
+            NetRegimeWC._convert_parent(R)
+        return L
+
+    @classmethod
+    def from_rates_mat(
+            cls,
+            pop_names: List[str],
+            rates_mat: np.ndarray  # pops x regimes
+            ) -> 'NetRegimeWCList':
+        """Creates from (pops x regimes) matrix. """
+        L = NetRegime1DList.from_regimes_mat(pop_names, rates_mat)
+        return cls._convert_parent(L)
+    
+    @classmethod
+    def from_xr(
+            cls,
+            R: xr.DataArray,
+            pop_dim: str | None = None,
+            regime_dim: str | None = None
+            ) -> 'NetRegimeWCList':
+        """Creates from 2D xarray. """
+        L = NetRegime1DList.from_xr(R, pop_dim, regime_dim)
+        return cls._convert_parent(L)
