7424b24e63d7556a52eb51a96b021ddd07830482
niknovikov19 <nikknovikov@gmail.com>
2025-06-24 15:04:12 -0400

    Use RateModel in response prediction

--- proto/response_pred/rate_model.py
+++ proto/response_pred/rate_model.py
@@ -11,7 +11,7 @@ def _to_array(x, shape):
         return np.array(x).reshape(shape)
 
 
-class RateModelBase(ABC):
+class RateModel(ABC):
 
     npops: int
     W: np.ndarray   # (npops, npops)
@@ -30,7 +30,7 @@ class RateModelBase(ABC):
     @abstractmethod
     def gain(self,
              mu: np.ndarray | float,
-             pop_num: int | None
+             pop_num: int | None = None
              ) -> np.ndarray | float:
         pass
 
@@ -47,17 +47,18 @@ class RateModelBase(ABC):
         # Time bins
         tvec = np.arange(0, nsteps * dt, dt)
 
-        # Initial state
-        R[:, 0] = r0[:, None]
+        # To column vectors
+        r0 = r0.reshape((self.npops, 1))
+        h = h.reshape((self.npops, 1))
 
-        # To column vector
-        h = h[:, None]
+        # Initial state
+        R[:, [0]] = r0
 
         # Iterate to find the steady state
         for n in range(1, nsteps):
-            r = R[:, n - 1]
-            r_hat = self.gain_func(self.W @ r + h[:, None])
-            R[:, n] = r + (r_hat - r) * dt / self.tau
+            r = R[:, [n - 1]]
+            r_hat = self.gain(self.W @ r + h)
+            R[:, [n]] = r + (r_hat - r) * dt / self.tau
         
         # Convert the result to xarray
         R = xr.DataArray(
@@ -81,29 +82,33 @@ class RateModelBase(ABC):
         # Time bins
         tvec = np.arange(0, nsteps * dt, dt)
 
+        # To column vector
+        r0 = r0.reshape((self.npops, 1))
+
         # Initial state
         R[0] = r0[pop_num]
 
-        # To column vector
-        r0 = r0[:, None]
+        # Steady-state
+        r_hat = self.gain(
+            self.W[pop_num, :] @ r0 + h, pop_num)
 
         # Iterate to find the steady state
         for n in range(1, nsteps):
             r = R[n - 1]
-            r_hat = self.gain_func(self.W[pop_num, :] @ r0 + h)
-            R[n] = r + (r_hat - r) * dt / self.tau
+            R[n] = r + (r_hat - r) * dt / self.tau[pop_num]
         
         # Convert the result to xarray
         R = xr.DataArray(
             R.reshape((1, -1)),
             dims=['pop', 'time'],
-            coords={'pop': pop_num, 'time': tvec},
+            coords={'pop': [pop_num], 'time': tvec},
         )
         return R
         
 
-class RateModelWC(RateModelBase):
+class RateModelWC(RateModel):
 
+    rmax: np.ndarray   # (npops, 1)
     gain_slope: np.ndarray   # (npops, 1)
     gain_center: np.ndarray   # (npops, 1)
     
@@ -111,41 +116,24 @@ class RateModelWC(RateModelBase):
             self,
             W: np.ndarray,
             tau: np.ndarray | float,
+            rmax: np.ndarray | float,
             gain_slope: np.ndarray | float,
             gain_center: np.ndarray | float
             ):
         super().__init__(W, tau)
+        self.rmax = _to_array(rmax, (self.npops, 1))
         self.gain_slope = _to_array(gain_slope, (self.npops, 1))
         self.gain_center = _to_array(gain_center, (self.npops, 1))
 
-    @abstractmethod
     def gain(self,
-             mu: np.ndarray | float,
-             pop_num: int | None
+             mu: np.ndarray | float,   # (npops x nsamples) or (1 x nsamples)
+             pop_num: int | None = None
              ) -> np.ndarray | float:
-        pass
-    
-    def gain_func(self, x: float | np.ndarray) -> float | np.ndarray:
-        k, xc  = self.gain_slope, self.gain_center
-        return self.rmax / (1 + np.exp(-k * (x - xc)))
-    
-    
-    
-    def run_1pop(self,
-            pop_num: int,
-            h_input: float,
-            r0: np.ndarray,
-            alpha: float = 1,
-            nsteps: int = 10,
-            ) -> tuple[float, np.ndarray]:
-        r0 = r0.reshape(-1, 1)
-        W = self.weights_mat[pop_num, :]
-        R = np.zeros((1, nsteps))
-        R[0] = r0[pop_num]
-        r = r0.copy()
-        for n in range(1, nsteps):
-            rpop = R[n - 1]
-            r[pop_num] = rpop
-            rpop_new = self.gain_func(W @ r + h_input)
-            R[n] = (1 - alpha) * rpop + alpha * rpop_new
-        return R[-1], R
+        rmax, k, c  = self.rmax, self.gain_slope, self.gain_center
+        if pop_num is None:
+            mu = mu.reshape((self.npops, -1))
+            return rmax / (1 + np.exp(-k * (mu - c)))
+        else:
+            mu = mu.reshape((1, -1))
+            rmax, k, c = rmax[pop_num], k[pop_num], c[pop_num]
+            return rmax / (1 + np.exp(-k * (mu - c)))    
